c5dc711020185d54c5a00bb18d2e5713a6c60d71
James Conry <james.conry@comcast.net>
2019-11-07 21:32:19 -0600

    adds user to database

--- models/example.js
+++ models/example.js
@@ -1,7 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
+  var Example = sequelize.define(
+    "Example",
+    {
+      text: DataTypes.STRING,
+      description: DataTypes.TEXT
+    },
+    { timestamps: false }
+  );
   return Example;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,58 @@
-var db = require("../models");
-
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
+  var db = require("../models");
+  app.post("/api/user", function(req, res) {
+    var data = [];
+    db.User.findAll({
+      where: {
+        email: "fake@fake.com" //req.body.email
+      }
+    })
+      .then(function(dbExamples) {
+        return (data = dbExamples);
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+    // for (var i = 0; i < users.length; i++) {
+
+    // }
+    console.log(data);
+    if (data.length > 0) {
+      res.json("Email already exists!");
+    } else {
+      db.User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+      }).then(function(dbExample) {
+        res.json(dbExample);
+      });
+    }
+  });
+
+  app.get("/api/users", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
+
+  // // Create a new example
+  // app.post("/api/examples", function(req, res) {
+  //   db.User.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
