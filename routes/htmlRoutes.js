36d9fd369b43621532d8357566cfdc64311d26ac
James Conry <james.conry@comcast.net>
2019-11-09 12:10:13 -0600

    added html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,17 +11,14 @@ module.exports = function(app) {
   });
 
   app.get("/movies", function(req, res) {
-    res.render("index");
+    res.render("movies");
   });
-  // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-
+  app.get("/shopping", function(req, res) {
+    res.render("shopping");
+  });
+  app.get("/soundtracks", function(req, res) {
+    res.render("soundtracks");
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
