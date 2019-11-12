7b9f8ea4d190e48fea91d2ba540afc1bcb6034a3
Charlie Crocker <cscrocker84@gmail.com>
2019-11-12 15:34:10 -0600

    Got Axios figured out, need to populate now

--- routes/movieApi.js
+++ routes/movieApi.js
@@ -1,31 +1,10 @@
-var createRow = function(data) {
-    // Create a new table row element
-    var tRow = $("<tr>");
-
-    // Methods run on jQuery selectors return the selector they we run on
-    // This is why we can create and save a reference to a td in the same statement we update its text
-    var titleTd = $("<td>").text(data.Title);
-    var yearTd = $("<td>").text(data.Year);
-    var actorsTd = $("<td>").text(data.Actors);
-      
-    // Append the newly created table data to the table row
-    tRow.append(titleTd, yearTd, actorsTd);
-    // Append the table row to the table body
-    $("tbody").append(tRow);
-  };
-
-  // The search OMDB function takes a movie, searches the omdb api for it, and then passes the data to createRow
-  var searchOMDB = function(movie) {
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      createRow(response);
-    });
-  };
-
-  // Search the OMDB API for the following movies, and append table rows for each
-  searchOMDB("Lord of the Rings");
-  searchOMDB("Two Towers");
-  searchOMDB("Return of the King");
+const axios = require('axios');
+ 
+// Make a request for a user with a given ID
+axios.get('http://www.omdbapi.com/?i=tt0120737&apikey=a59277f6')
+  .then(function (response) {
+    console.log(response.data.Title);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
