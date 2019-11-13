6ae336b9423fde319ec9e9d700b4e4dcb7e78d91
Charlie Crocker <cscrocker84@gmail.com>
2019-11-12 19:48:19 -0600

    temporary fix for movies

--- routes/movieApi.js
+++ routes/movieApi.js
@@ -8,3 +8,19 @@ axios.get('http://www.omdbapi.com/?i=tt0120737&apikey=a59277f6')
   .catch(function (error) {
     console.log(error);
   });
+
+  axios.get('http://www.omdbapi.com/?i=tt0167261&apikey=a59277f6')
+  .then(function (response) {
+    console.log(response.data.Title);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  axios.get('http://www.omdbapi.com/?i=tt0405185&apikey=a59277f6')
+  .then(function (response) {
+    console.log(response.data.Title);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
